05738f5e33c5657e0cb5ec3b9898b42f0fcd94ce
jordyderuijter@gmail.com <jordyderuijter@gmail.com@555aa13e-6e28-5953-3872-e14ca3dd7517>
2012-12-27 18:19:53 +0000

    Main page beginnetje gemaakt, language knopt is half functioneel: plaatje veranderd wel maar de taal nog niet.

--- trunk/StadNav/StadNav/MainPage.xaml.cs
+++ trunk/StadNav/StadNav/MainPage.xaml.cs
@@ -16,60 +16,32 @@ namespace StadNav
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        private double windowHeight, windowWidth;
+        private Boolean dutch;
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-            this.Content.UpdateLayout();
-            //System.Diagnostics.Debug.WriteLine(System.Windows.Application.Current.Host.Content.ActualHeight);
-            this.windowHeight = System.Windows.Application.Current.Host.Content.ActualHeight;
-            this.windowHeight = System.Windows.Application.Current.Host.Content.ActualWidth;
+            dutch = true;
+        }
 
-            StackPanel topBar = new StackPanel();
-            topBar.Orientation = System.Windows.Controls.Orientation.Horizontal;
-            topBar.Name = "topBar";
-            topBar.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
-            topBar.Orientation = System.Windows.Controls.Orientation.Horizontal;
-            topBar.Margin = new System.Windows.Thickness { Left = 12, Top = 3, Right = 0, Bottom = 10 };
-
-            TextBlock title = new TextBlock();
-            title.Name = "title";
-            title.Text = "StadNav";
-            title.Width = 100;
-            title.TextAlignment = TextAlignment.Left;
-            title.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-            title.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-
-            //TODO: CHANGE THESE IMAGES
-            Image flagButton = new Image();         //THIS IS GOING TO BE THE FLAG BUTTON
-            flagButton.Source = new BitmapImage(new Uri("images/dummy.jpg", UriKind.Relative));
-            flagButton.Width = 100;
-            flagButton.Height = 30;
-            flagButton.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-            flagButton.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-            flagButton.Margin = new System.Windows.Thickness { Left = 100 };
-            //END FLAG BUTTON
-
-            Image helpButton = new Image();         //THIS IS GOING TO BE THE HELP BUTTON
-            helpButton.Source = new BitmapImage(new Uri("images/dummy.jpg", UriKind.Relative));
-            helpButton.Width = 100;
-            helpButton.Height = 30;
-            helpButton.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-            helpButton.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-            helpButton.Margin = new System.Windows.Thickness { Left = 5 };
-            //END HELP BUTTON
-
-            topBar.Children.Add(title);
-            topBar.Children.Add(flagButton);
-            topBar.Children.Add(helpButton);
-
-            this.Content = topBar;
-
-            /*
-            Button help = new Button();
-            Button language = new Button();
-             */
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            //Changes language
+            /*Button button = sender as Button;
+            Image content = button.Content as Image;
+            if (content != null)
+            {*/
+                if (dutch)
+                {
+                    ((Image) button5.Content).Source = new BitmapImage(new Uri("images/eng.jpg", UriKind.Relative));
+                    dutch = false;
+                }
+                else
+                {
+                    ((Image)button5.Content).Source = new BitmapImage(new Uri("images/ned.jpg", UriKind.Relative));
+                    dutch = true;
+                }
+           // }
         }
     }
 }
