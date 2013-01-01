6435ce62723a5624ff4cbfc057bb70851bf52645
jordyderuijter@gmail.com <jordyderuijter@gmail.com@555aa13e-6e28-5953-3872-e14ca3dd7517>
2013-01-01 21:51:56 +0000

    Taal op alle pagina's en route + waypoint klassen.

---
+++ StadNav/StadNav/WayPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace StadNav
+{
+    public class Waypoint
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+
+        public Waypoint(double latitude, double longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+    }
+}
