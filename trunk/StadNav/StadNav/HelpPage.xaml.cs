80a8de2102c65a3cd2227ec7af95f12a281dc7bd
ajpserva@avans.nl <ajpserva@avans.nl@555aa13e-6e28-5953-3872-e14ca3dd7517>
2013-01-03 21:17:15 +0000

    + Route Rating Page + Button on Main page AFTER loading a route. + Help page description about the Route Rating Page Dutch + English + User indicating pushpin is now bigger and can be indicated in English and in Dutch + Help page scrolling functionality added

--- trunk/StadNav/StadNav/HelpPage.xaml.cs
+++ trunk/StadNav/StadNav/HelpPage.xaml.cs
@@ -36,22 +36,32 @@ namespace StadNav
             if (!(bool)PhoneApplicationService.Current.State["language"])
             {
                 ((Image)button5.Content).Source = new BitmapImage(new Uri("images/eng.jpg", UriKind.Relative));
+                textBlock1.TextWrapping = TextWrapping.Wrap;
                 textBlock1.Text = "When the application starts you will see a map." +
                     "To load a route onto the map, press the route button." +
                     "On the next page you will see a list of routes which you can choose by pressing on it." +
                     "After choosing a route you will be sent back to the main page." +
                     "The route will now be loaded onto the map and your own position will be showed by an arrow sign." +
-                    "You can now follow the route on the map. If you would like to change the language of the application, press the flag in the top right corner.";
+                    "You can now follow the route on the map. If you would like to change the language of the application, press the flag in the top right corner."+
+                    "\n \n If you tap on the button with a star on it you will be able to rate the route you are currently following." +
+                    "\n You do this by clicking on the stars that appear. After pressing a star the amount of stars you have rated the route with will  be displayed."+
+                    "\n When you are done with rating the route, tap on the 'Rate' button in order to save your rating and go back to the map.";
+                textBlock1.Height = textBlock1.Text.Count();
             }
             else
             {
                 ((Image)button5.Content).Source = new BitmapImage(new Uri("images/ned.jpg", UriKind.Relative));
+                textBlock1.TextWrapping = TextWrapping.Wrap;
                 textBlock1.Text = "Wanneer de applicatie gestart wordt ziet u een kaart." +
                     " Om een route op deze kaart te laden klikt u op de knop route." +
                     " Hierna wordt u naar een andere pagina geleid waar u kunt kiezen uit een van de beschikbare routes door er op de drukken." +
                     " Als u een route heeft gekozen wordt u weer terug geleid naar de hoofdpagina." +
                     " Nu is de route op de kaart geladen en ziet u uw positie aangegeven met een pijl." +
-                    " U kunt nu de route volgen met de kaart. Als u de taal van de applicatie wilt veranderen klikt u op het vlaggetje in de rechterboven hoek";
+                    " U kunt nu de route volgen met de kaart. Als u de taal van de applicatie wilt veranderen klikt u op het vlaggetje in de rechterboven hoek." +
+                    " \n \n Als je op de knop met een ster drukt wordt je naar een beoordelingspagina toeverwezen. Op dezepagina kun je de gekozen route beoordelen." +
+                    " Dit doe je met het drukken op de sterren die aanwezig zijn op de pagina. Bij het drukken van een ster wordt aangegeven hoeveel sterren je aan,"+
+                    " een route geeft. Wanneer je klaar bent druk je op Beoordelen om je beoordeling op te slaan en terug te gaan naar de kaart.";
+               textBlock1.Height = textBlock1.Text.Count();
             }
         }
     }
---
+++ trunk/StadNav/StadNav/RouteRatingPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace StadNav
+{
+    public partial class RouteRatingPage : PhoneApplicationPage
+    {
+        public RouteRatingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            NavigationService.Navigate(new Uri("/MainPage.XAML", UriKind.Relative));
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock2.Text = "1";
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock2.Text = "2";
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock2.Text = "3";
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock2.Text = "4";
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock2.Text = "5";
+        }
+
+        #region Button images exception catching methods
+        private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_ImageFailed_1(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_ImageFailed_2(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_ImageFailed_3(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_ImageFailed_4(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+    }
+}
