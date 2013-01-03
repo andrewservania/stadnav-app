83783eac0718b7c4d075aee8f13a61bef4881cd5
remco.plasmeijer@gmail.com <remco.plasmeijer@gmail.com@555aa13e-6e28-5953-3872-e14ca3dd7517>
2013-01-03 14:35:28 +0000

    v0.000128934982 van navigatie.

--- trunk/StadNav/StadNav/MainPage.xaml.cs
+++ trunk/StadNav/StadNav/MainPage.xaml.cs
@@ -15,6 +15,9 @@ using Microsoft.Phone.Shell;
 using Microsoft.Phone.Controls.Maps;
 using System.Device.Location;
 using Microsoft.Phone.Tasks;
+using System.Collections.ObjectModel;
+using StadNav.RouteService;
+using StadNav.GeocodeService;
 
 namespace StadNav
 {
@@ -29,6 +32,10 @@ namespace StadNav
         GeoCoordinateWatcher coordinateWatcher;
         bool checker = false;
         static bool trigger = false;
+        Pushpin userPushpin = new Pushpin();
+        int defaultZoom = 15;
+
+        internal GeocodeService.GeocodeResult[] geocodeResults;
 
         // Constructor
         public MainPage()
@@ -56,6 +63,14 @@ namespace StadNav
             map1.Children.Add(routePolyLine);
             pushPinLayer = new MapLayer();
             map1.Children.Add(pushPinLayer);
+            map1.ZoomLevel = defaultZoom;
+
+            //init user's pushpin
+            userPushpin.Name = "User";
+            userPushpin.Background = new SolidColorBrush(Colors.Blue);
+            userPushpin.Content = "User";
+            pushPinLayer.AddChild(userPushpin, new GeoCoordinate(0,0));
+
             coordinateWatcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
             coordinateWatcher.PositionChanged += HandleGeoPositionChanged;
             coordinateWatcher.Start();
@@ -98,11 +113,104 @@ namespace StadNav
             }
         }
 
+        private void CalculateRoute(List<GeoCoordinate> locations)
+        {
+            // Create the service variable and set the callback method using the CalculateRouteCompleted property.
+            RouteService.RouteServiceClient routeService = new RouteService.RouteServiceClient("BasicHttpBinding_IRouteService");
+            routeService.CalculateRouteCompleted += new EventHandler<RouteService.CalculateRouteCompletedEventArgs>(routeService_CalculateRouteCompleted);
+
+            // Set the token.
+            RouteService.RouteRequest routeRequest = new RouteService.RouteRequest();
+            routeRequest.Credentials = new Credentials();
+            routeRequest.Credentials.ApplicationId = ((ApplicationIdCredentialsProvider)map1.CredentialsProvider).ApplicationId;
+
+            // Return the route points so the route can be drawn.
+            routeRequest.Options = new RouteService.RouteOptions();
+            routeRequest.Options.RoutePathType = RouteService.RoutePathType.Points;
+
+            // Set the waypoints of the route to be calculated using the Geocode Service results stored in the geocodeResults variable.
+            routeRequest.Waypoints = new System.Collections.ObjectModel.ObservableCollection<RouteService.Waypoint>();
+            foreach (GeoCoordinate result in locations)
+            {
+                StadNav.RouteService.Waypoint wp = new StadNav.RouteService.Waypoint();
+                wp.Location = new GeoCoordinate(result.Latitude, result.Longitude);
+                routeRequest.Waypoints.Add(wp);
+            }
+
+            // Make the CalculateRoute asnychronous request.
+            routeService.CalculateRouteAsync(routeRequest);
+        }
+
+        private void routeService_CalculateRouteCompleted(object sender, RouteService.CalculateRouteCompletedEventArgs e)
+        {
+
+            // If the route calculate was a success and contains a route, then draw the route on the map.
+            if ((e.Result.ResponseSummary.StatusCode == RouteService.ResponseStatusCode.Success) & (e.Result.Result.Legs.Count != 0))
+            {
+                // Set properties of the route line you want to draw.
+                Color routeColor = Colors.Blue;
+                SolidColorBrush routeBrush = new SolidColorBrush(routeColor);
+                MapPolyline routeLine = new MapPolyline();
+                routeLine.Locations = new LocationCollection();
+                routeLine.Stroke = routeBrush;
+                routeLine.Opacity = 0.65;
+                routeLine.StrokeThickness = 5.0;
+
+                // Retrieve the route points that define the shape of the route.
+                foreach (GeoCoordinate p in e.Result.Result.RoutePath.Points)
+                {
+                    routeLine.Locations.Add(new GeoCoordinate(p.Latitude, p.Longitude));
+                }
+
+                // Add a map layer in which to draw the route.
+                MapLayer myRouteLayer = new MapLayer();
+                map1.Children.Add(myRouteLayer);
+
+                // Add the route line to the new layer.
+                myRouteLayer.Children.Add(routeLine);
+
+                // Figure the rectangle which encompasses the route. This is used later to set the map view.
+                //LocationRect rect = new LocationRect(routeLine.Locations[0], routeLine.Locations[routeLine.Locations.Count - 1]);
+
+                // For each geocode result (which are the waypoints of the route), draw a dot on the map.
+                foreach (GeocodeService.GeocodeResult gr in geocodeResults)
+                {
+                    Ellipse point = new Ellipse();
+                    point.Width = 10;
+                    point.Height = 10;
+                    point.Fill = new SolidColorBrush(Colors.Red);
+                    point.Opacity = 0.65;
+                    GeoCoordinate location = new GeoCoordinate(gr.Locations[0].Latitude, gr.Locations[0].Longitude);
+                    MapLayer.SetPosition(point, location);
+                    MapLayer.SetPositionOrigin(point, PositionOrigin.Center);
+
+                    // Add the drawn point to the route layer.                    
+                    myRouteLayer.Children.Add(point);
+                }
+
+                // Set the map view using the rectangle which bounds the rendered route.
+                //map1.SetView(rect);
+            }
+        }
+
+        private RouteService.Waypoint GeocodeResultToWaypoint(GeocodeService.GeocodeResult result)
+        {
+            RouteService.Waypoint waypoint = new RouteService.Waypoint();
+            waypoint.Description = result.DisplayName;
+            waypoint.Location = new GeoCoordinate();
+            waypoint.Location.Latitude = result.Locations[0].Latitude;
+            waypoint.Location.Longitude = result.Locations[0].Longitude;
+            return waypoint;
+        }
+
         public void refreshMap()
         {
             if (checker == false) // To avoid redrawing the routeline and for performance improvement
             {
-
+                List<GeoCoordinate> locations = new List<GeoCoordinate>();
+                locations.Add(userPushpin.Location);
+                bool nextLocation = false;
+                
                 foreach (Waypoint wp in currentRoute.Waypoints)
                 {
                     routePolyLine.Locations.Add(new GeoCoordinate(wp.Latitude, wp.Longitude));
@@ -112,10 +220,20 @@ namespace StadNav
                     pin.Background = new SolidColorBrush(Colors.Orange);
                     pin.MouseEnter += OnTapEnter;
                     pin.MouseLeave += OnTapLeave;
-                    pushPinLayer.AddChild(pin, new GeoCoordinate(wp.Latitude, wp.Longitude));
+                    GeoCoordinate waypointCoords = new GeoCoordinate(wp.Latitude, wp.Longitude);
+                    pushPinLayer.AddChild(pin, waypointCoords);
+                    if (!nextLocation)
+                    {
+                        nextLocation = true;
+                        locations.Add(waypointCoords);
+                    }
                 }
-                map1.Center = new GeoCoordinate(currentRoute.Waypoints.First().Latitude, currentRoute.Waypoints.First().Longitude);
-                map1.ZoomLevel = 15;
+                CalculateRoute(locations);
+
+                map1.SetView(LocationRect.CreateLocationRect(locations));
+
+                //map1.Center = new GeoCoordinate(currentRoute.Waypoints.First().Latitude, currentRoute.Waypoints.First().Longitude);
+                map1.ZoomLevel = defaultZoom;
                 checker = true;
             }           
         }
@@ -185,6 +303,13 @@ namespace StadNav
             pushpin.RenderTransform = st;
         }
 
+        //updates user's pushpin
+        public void updateUserPushpin(Double Lat, Double Long)
+        {
+            GeoCoordinate location = new GeoCoordinate(Lat, Long);
+            userPushpin.Location = location;
+        }
+
         public void HandleGeoPositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
         {
             if (!e.Position.Location.IsUnknown)
@@ -192,6 +317,7 @@ namespace StadNav
                 Double Latitude = e.Position.Location.Latitude;
                 Double Longitude = e.Position.Location.Longitude;
                 map1.Center = new GeoCoordinate(Latitude, Longitude);
+                updateUserPushpin(Latitude, Longitude);
             }
             
         }
