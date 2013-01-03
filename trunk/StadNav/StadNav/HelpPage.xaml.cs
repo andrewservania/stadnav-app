d91074318669a2d4ec3f6840892b33a4a49456f7
jordyderuijter@gmail.com <jordyderuijter@gmail.com@555aa13e-6e28-5953-3872-e14ca3dd7517>
2013-01-03 14:54:45 +0000

    Help page filled.

--- trunk/StadNav/StadNav/HelpPage.xaml.cs
+++ trunk/StadNav/StadNav/HelpPage.xaml.cs
@@ -35,11 +35,23 @@ namespace StadNav
         {
             if (!(bool)PhoneApplicationService.Current.State["language"])
             {
-                ((Image)button5.Content).Source = new BitmapImage(new Uri("images/eng.jpg", UriKind.Relative));  
+                ((Image)button5.Content).Source = new BitmapImage(new Uri("images/eng.jpg", UriKind.Relative));
+                textBlock1.Text = "When the application starts you will see a map." +
+                    "To load a route onto the map, press the route button." +
+                    "On the next page you will see a list of routes which you can choose by pressing on it." +
+                    "After choosing a route you will be sent back to the main page." +
+                    "The route will now be loaded onto the map and your own position will be showed by an arrow sign." +
+                    "You can now follow the route on the map. If you would like to change the language of the application, press the flag in the top right corner.";
             }
             else
             {
                 ((Image)button5.Content).Source = new BitmapImage(new Uri("images/ned.jpg", UriKind.Relative));
+                textBlock1.Text = "Wanneer de applicatie gestart wordt ziet u een kaart." +
+                    " Om een route op deze kaart te laden klikt u op de knop route." +
+                    " Hierna wordt u naar een andere pagina geleid waar u kunt kiezen uit een van de beschikbare routes door er op de drukken." +
+                    " Als u een route heeft gekozen wordt u weer terug geleid naar de hoofdpagina." +
+                    " Nu is de route op de kaart geladen en ziet u uw positie aangegeven met een pijl." +
+                    " U kunt nu de route volgen met de kaart. Als u de taal van de applicatie wilt veranderen klikt u op het vlaggetje in de rechterboven hoek";
             }
         }
     }
